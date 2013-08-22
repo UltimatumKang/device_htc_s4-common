52a3fb289d97de4481b4fb04c3a4c1bb6bd71833
Hieu Phan <aznrice2k4@gmail.com>
2013-08-21 20:54:43 -0500

    changes for m7 ported kernel
    Signed-off-by: Hieu Phan <aznrice2k4@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,8 +39,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 TARGET_KERNEL_VERSION := 3.4
-TARGET_KERNEL_SOURCE := kernel/htc/msm8960-3.4
-TARGET_KERNEL_CUSTOM_LINARO_TOOLCHAIN := custom-4.8/bin/arm-cortexa9_neon-linux-gnueabihf-
+TARGET_KERNEL_SOURCE := kernel/htc/evitaul
+TARGET_KERNEL_CUSTOM_LINARO_TOOLCHAIN := arm-eabi-4.7/bin/arm-eabi-
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true
